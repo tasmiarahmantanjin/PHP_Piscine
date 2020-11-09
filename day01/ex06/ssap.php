0c4350c3e9ff70891cf29cbc6a2e04cf0d830716
Tasmia Rahman <tasmiata@gmail.com>
2020-11-09 20:04:23 +0200

    solution of ex06

--- day01/ex06/ssap.php
+++ day01/ex06/ssap.php
@@ -1,22 +1,14 @@
-
 #!/usr/bin/php
-<?php
-	$i = 1;
-	$arr = array();
-	foreach ($argv as $value)
-	{
-		if ($i++ > 1)
-		{
-			$str = preg_split("/ +/", trim($value));
-			if ($str[0] != "")
-			{
-				$arr = array_merge($arr, $str);
-			}
-		}
-	}
-	sort($arr);
-	foreach($arr as $elements)
-	{
-		echo "$elements"."\n";
-	}
+<?PHP
+unset($argv[0]);	//deleting a single array element
+$arr = array();
+foreach ($argv as $value_1)
+{
+	$str = array_filter(explode(' ', $value_1));
+	foreach ($str as $value_2)
+		$arr[] = $value_2;
+}
+sort($arr);
+foreach ($arr as $value_1)
+	echo $value_1 . "\n";
 ?>
