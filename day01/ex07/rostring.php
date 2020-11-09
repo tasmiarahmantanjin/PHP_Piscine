afe9761f2e5111621117c0bc294c862919fa6540
Tasmia Rahman <tasmiata@gmail.com>
2020-11-09 20:04:38 +0200

    solution of ex07

--- day01/ex07/rostring.php
+++ day01/ex07/rostring.php
@@ -2,11 +2,10 @@
 <?php
 	if ($argc == 1)
 	exit();
-$arr = explode(" ", $argv[1]); //split the eliments
-$arr = array_filter($arr); //filter the arr
-$first_element = array_shift($arr); //split the 1st element
-array_push($arr, $first_element);
-$str = implode(" ", $arr);
-if (!empty($str))
-	echo "$str\n";
+	$arr = array_filter(explode(" ", $argv[1])); //split the eliments
+	$first_elem = array_shift($arr); //split the 1st element
+	array_push($arr, $first_elem);
+	$str = implode(" ", $arr);
+	if (!empty($str))
+		print "$str\n";
 ?>
