6e346d35403ac45139922cf3d76f96eb80e90664
Tasmia Rahman <tasmiata@gmail.com>
2020-11-10 23:03:22 +0200

    update

--- day01/ex08/main.php
+++ day01/ex08/main.php
@@ -1,13 +1,11 @@
 #!/usr/bin/php
-
 <?php
 include("ft_is_sort.php");
 
-$tab = array("B", "A", "C");
+//$tab = array("B", "A", "C");
 
 if (ft_is_sort($tab))
 	echo "The array is sorted\n";
 else
 	echo "The array is not sorted\n";
-
 ?>
