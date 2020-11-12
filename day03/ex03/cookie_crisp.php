a4e1ebb3514aa734cb4b3d82487a4c246a94d187
Tasmia Rahman <tasmiata@gmail.com>
2020-11-12 22:10:42 +0200

    delete old stuff

--- day03/ex03/cookie_crisp.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_GET["action"] == "set")
-	setcookie($_GET["name"], $_GET["value"], time() + 3600);
-else if ($_GET["action"] == "get")
-	echo  $_COOKIE[$_GET["name"]];
-else if ($_GET["action"] == "del")
-	setcookie($_GET["name"], null, -1, '/');
-?>
--- day03/ex05/read_img.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: image/png');
-readfile("/goinfre/mhasan/mamp/apache2/htdocs/d03/img/42.png");
-?>
