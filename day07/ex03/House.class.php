f3cdd54a6ffb8b070ad1f982f41edef3d0ef5b4c
Tasmia Rahman <tasmiata@gmail.com>
2020-11-24 15:12:27 +0200

    solution of ex03

---
+++ day07/ex03/House.class.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract class House
+{
+	abstract function getHouseName();
+	abstract function getHouseMotto();
+	abstract function getHouseSeat();
+	function introduce()
+	{
+		print "House " . $this->getHouseName() . " of " . $this->getHouseSeat() . " : " . '"' . $this->getHouseMotto() . '"' . PHP_EOL;
+	}
+}
+
+?>
